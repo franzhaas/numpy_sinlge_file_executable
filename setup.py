e5217fd8c6bcf80bdca0723d5e02154c944aa834
franzhaas <franz.dominik.haas@gmail.com>
2022-01-10 22:48:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ from distutils.core import setup, Extension
 module1 = Extension('spam_no_extra_incref',
                     sources = ['food2.c'])
 
-module2 = Extension('spam_no_extra_incref',
+module2 = Extension('spam_extra_incref',
                     sources = ['food.c'])
 
 
 #setup(name="cython_example_fib", ext_modules=cythonstuff, compiler_directives={'embedsignature': True, "emit_code_comments": True})
-setup(name="food", ext_modules = [module1] )
+setup(name="food", ext_modules = [module1, module2] )
